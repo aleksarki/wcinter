dcf9d8c47d19be3077ad4b1e86db4d79b04f6451
aleksarki <sashas200@yandex.ru>
2026-07-07 16:19:05 +0500

    broke definitions header into several

---
+++ source/include/definitions/structs.hpp
@@ -0,0 +1,90 @@
+#pragma once
+#ifndef CINTER_INCLUDE_DEFINITIONS_STRUCTS_HPP
+#define CINTER_INCLUDE_DEFINITIONS_STRUCTS_HPP
+
+#include "definitions/types.hpp"
+
+namespace wci
+{
+    struct Coord
+    {
+        Short x;
+        Short y;
+    };
+
+    struct SmallRect
+    {
+        Short left;    // X of top left corner.
+        Short top;     // Y of top left corner.
+        Short right;   // X of bottom right corner.
+        Short bottom;  // Y of bottom right corner.
+    };
+
+    struct CharInfo
+    {
+        wchar_t character;
+        Word attributes;
+    };
+
+    struct CursorInfo
+    {
+        Dword size;
+        bool visible;
+    };
+
+    struct ScreenBufferInfo
+    {
+        Coord size;
+        Coord cursorPosition;
+        Word attributes;
+        SmallRect window;
+        Coord maxWindowSize;
+    };
+
+    struct KeyEventRecord
+    {
+        bool keyDown;
+        Word repeatCount;
+        Word virtualKeyCode;
+        Word virtualScanCode;
+        wchar_t character;
+        Dword controlKeyState;
+    };
+
+    struct MouseEventRecord
+    {
+        Coord mousePosition;
+        Dword buttonState;
+        Dword controlKeyState;
+        Dword eventFlags;
+    };
+
+    struct WindowBufferSizeRecord
+    {
+        Coord size;
+    };
+
+    struct MenuEventRecord
+    {
+        unsigned int commandId;
+    };
+
+    struct FocusEventRecord
+    {
+        bool setFocus;
+    };
+
+    struct InputRecord
+    {
+        Word eventType;
+        union {
+            KeyEventRecord keyEvent;
+            MouseEventRecord mouseEvent;
+            WindowBufferSizeRecord windowBufferSizeEvent;
+            MenuEventRecord menuEvent;
+            FocusEventRecord focusEvent;
+        } event;
+    };
+}
+
+#endif  // CINTER_INCLUDE_DEFINITIONS_STRUCTS_HPP
---
+++ source/include/definitions/types.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef CINTER_INCLUDE_DEFINITIONS_TYPES_HPP
+#define CINTER_INCLUDE_DEFINITIONS_TYPES_HPP
+
+namespace wci
+{
+    using Short = short;
+    using Word = unsigned short;
+    using Dword = unsigned long;
+    using Handle = void*;
+}
+
+#endif  // CINTER_INCLUDE_DEFINITIONS_TYPES_HPP
